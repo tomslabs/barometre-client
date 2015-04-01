0395c6988fd7761110dcc05712491b86f566a389
Loïc Giraudel <lgiraudel@purch.com>
2015-04-01 18:05:07 +0200

    Fixing ajax form submission

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,26 @@
-var updateMoods = function () {
-    $.ajax({
-        url: 'https://purch-barometre.herokuapp.com/mood'
-    }).done(function(data) {
-        var $table = $('#mood-list');
-        $table.empty();
-        data.forEach(function(elem) {
-            var date = moment(elem.createdAt);
-            $table.append('<tr><td>' + elem.email + '</td><td>' + elem.mood + '</td><td>' + date.format('YYYY-MM-DD HH:mm:SS') + '</td></tr>');
-        });
-    });
-};
-
 $(function() {
+    var updateMoods = function () {
+        $.ajax({
+            url: 'https://purch-barometre.herokuapp.com/mood'
+        }).done(function(data) {
+            var $table = $('#mood-list');
+            $table.empty();
+            data.forEach(function(elem) {
+                var date = moment(elem.createdAt);
+                $table.append('<tr><td>' + elem.email + '</td><td>' + elem.mood + '</td><td>' + date.format('YYYY-MM-DD HH:mm:SS') + '</td></tr>');
+            });
+        });
+    };
+
     updateMoods();
-    $('#mood-form').ajaxForm(function() { 
-        updateMoods();
+
+    $('#mood-form').submit(function(e) {
+        e.preventDefault();
+
+        $(this).ajaxSubmit({
+            success: function() {
+                updateMoods();
+            }
+        });
     });
 });
